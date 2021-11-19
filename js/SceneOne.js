f3182f00700abd8debc2ded220b33bb2c5a662e8
Harry-Yates <hyates1@gmail.com>
2021-11-19 15:46:22 +0100

    finalised opening scene design

--- js/SceneOne.js
+++ js/SceneOne.js
@@ -9,6 +9,7 @@ export default class SceneOne extends Phaser.Scene {
     Player.preload(this);
     this.load.image("dirt", "./assets/images/map-environment/dirt.png");
     this.load.image("elements", "./assets/images/map-environment/elements.png");
+    this.load.image("resources", "./assets/images/map-environment/resources.png");
     this.load.tilemapTiledJSON("map", "./assets/images/map-environment/opening-scene-map.json");
   }
 
@@ -18,12 +19,17 @@ export default class SceneOne extends Phaser.Scene {
     const map = this.make.tilemap({ key: "map" });
     const groundDirt = map.addTilesetImage("dirt", "dirt", 32, 32, 0, 0);
     const groundObjects = map.addTilesetImage("elements", "elements", 32, 32, 0, 0);
+    const resources = map.addTilesetImage("resources", "resources", 32, 32, 0, 0);
     const layer1 = map.createStaticLayer("Tile Layer 1", groundDirt, 0, 0);
     const layer2 = map.createStaticLayer("Tile Layer 2", groundObjects, 0, 0);
+    const layer3 = map.createStaticLayer("Tile Layer 3", resources, 0, 0);
     layer1.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(layer1);
     layer2.setCollisionByProperty({ collides: true });
     this.matter.world.convertTilemapLayer(layer2);
+    layer3.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(layer3);
+
     this.player = new Player({ scene: this, x: 180, y: 480, texture: "main_character", frame: "u1" });
     this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
