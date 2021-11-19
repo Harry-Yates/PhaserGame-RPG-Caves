7d7ab654578f78bc26d9ac9422ef3cb57442754b
Harry-Yates <hyates1@gmail.com>
2021-11-19 10:44:01 +0100

    added map json to opening scene

--- js/SceneOne.js
+++ js/SceneOne.js
@@ -7,9 +7,14 @@ export default class SceneOne extends Phaser.Scene {
 
   preload() {
     Player.preload(this);
+    this.load.image("tiles", "./assets/images/map-environment/dirt.png");
+    this.load.tilemapTiledJSON("map", "./assets/images/map-environment/opening-scene-map.json");
   }
 
   create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("dirt", "tiles", 32, 32, 0, 0);
+    const layer1 = map.createStaticLayer("Tile Layer 1", tileset, 0, 0);
     this.player = new Player({ scene: this, x: 180, y: 480, texture: "main_character", frame: "u1" });
     this.player.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
