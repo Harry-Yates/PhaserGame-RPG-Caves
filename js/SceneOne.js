e82437efdc7860b48505c41569d7d7514ca6e005
Harry-Yates <hyates1@gmail.com>
2021-11-22 20:33:13 +0100

    set up portals

--- js/SceneOne.js
+++ js/SceneOne.js
@@ -16,6 +16,7 @@ export default class SceneOne extends Phaser.Scene {
     this.load.image("elements", "./assets/images/map-environment/elements.png");
     this.load.image("resources", "./assets/images/map-environment/resources.png");
     this.load.tilemapTiledJSON("map", "./assets/images/map-environment/opening-scene-map.json");
+    this.load.atlas("portal", "./assets/images/portal/portal.png", "./assets/images/portal/portal_atlas.json");
   }
 
   create() {
@@ -37,6 +38,9 @@ export default class SceneOne extends Phaser.Scene {
     this.matter.world.convertTilemapLayer(layer3);
     this.map.getObjectLayer("Treasure").objects.forEach((treasure) => new Treasure({ scene: this, treasure }));
     this.map.getObjectLayer("Enemies").objects.forEach((enemy) => this.enemies.push(new Enemy({ scene: this, enemy })));
+
+    this.addPortal();
+
     this.player = new Player({ scene: this, x: 180, y: 480, texture: "main_character", frame: "u1" });
     // this.player.setScale(1.5);
     this.player.inputKeys = this.input.keyboard.addKeys({
@@ -50,6 +54,25 @@ export default class SceneOne extends Phaser.Scene {
     // camera.startFollow(this.player);
     // camera.setLerp(0.1, 0.1);
     // camera.setBounds(0, 0, this.game.config.width, this.game.config.height);
+  }
+
+  //MOVE BETWEEN PORTALS
+  addPortal() {
+    const portal = this.map.getObjectLayer("Portal");
+    portal.objects.forEach((portal) => {
+      let portalItem = new Phaser.Physics.Matter.Sprite(this.matter.world, portal.x, portal.y, "portal", portal.type);
+      let yOrigin = portal.properties.find((p) => p.name == "yOrigin").value;
+      portalItem.x += portalItem.width / 2;
+      portalItem.y -= portalItem.height / 2;
+      portalItem.y = portalItem.y + portalItem.height * (yOrigin - 0.5);
+
+      const { Body, Bodies } = Phaser.Physics.Matter.Matter;
+      var circleCollider = Bodies.circle(portalItem.x, portalItem.y, 3, { isSensor: false, label: "collider" });
+      portalItem.setExistingBody(circleCollider);
+      portalItem.setStatic(true);
+      portalItem.setOrigin(0.5, yOrigin);
+      this.add.existing(portalItem);
+    });
   }
 
   update() {
