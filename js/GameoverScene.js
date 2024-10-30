bbb847abbec905313ac0ad8b938f299381914636
Harry <harry.yates@barstruck.com>
2024-10-30 14:54:37 +0100

    feat: touch controlls to reset the game

--- js/GameoverScene.js
+++ js/GameoverScene.js
@@ -20,6 +20,7 @@ class GameoverScene extends Phaser.Scene {
       start: Phaser.Input.Keyboard.KeyCodes.E,
     });
   }
+
   create() {
     var bg = this.add.sprite(0, 0, "background2");
     bg.setOrigin(0, 0);
@@ -29,30 +30,30 @@ class GameoverScene extends Phaser.Scene {
       fill: "#fff",
     });
 
-    // var play = this.add.sprite(193, 350, "play_button");
-    // bg.setOrigin(0, 0);
-    // play.setScale(0.2);
-
-    // var text = this.add.text(77, 100, "CAVES");
-    // text.setOrigin(0, 0);
-    // text.setScale(5);
-
-    // text.setOrigin(0, 0);
-    // text.setScale(1);
-
     var music = this.sound.add("gameover");
     music.play();
+
+    // Add touch input to reset the game
+    this.input.on("pointerdown", () => {
+      this.resetGame();
+    });
+
+    // Existing keyboard input to reset the game
+    this.input.keyboard.on("keydown-E", () => {
+      this.resetGame();
+    });
   }
 
   update() {
     if (this.inputKeys.start.isDown) {
-      // console.log("buttonE is pressed");
-      // this.registry.destroy(); // destroy registry
-      // this.events.off();
-      this.scene.start("MenuScene");
-      this.sound.get("gameover").stop();
-      window.location.reload();
+      this.resetGame();
     }
+  }
+
+  resetGame() {
+    this.scene.start("MenuScene");
+    this.sound.get("gameover").stop();
+    window.location.reload();
   }
 
   shutdown() {
